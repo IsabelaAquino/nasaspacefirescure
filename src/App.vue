fa5566c714613609d4733a4ab4da722d06614878
Isabela Aquino <isabela.isavzp@gmail.com>
2019-10-19 16:49:16 -0300

    mapa e estrutura do projeto

---
+++ src/App.vue
@@ -0,0 +1,82 @@
+<template>
+    <div class="row">
+        <flNavBar />
+        <div id="map" class="col-md-12 map">
+
+        </div>
+    </div>
+</template>
+
+<script>
+import {LMap, LTileLayer, LMarker} from 'vue2-leaflet'
+import flNavBar from './components/navbar/main.vue'
+
+export default {
+  name: 'app',
+  components: {
+    flNavBar
+  },
+
+  mounted() {
+    this.getMapa()
+  },
+
+  methods: {
+    getMapa(){
+      zoomControl: false
+      attributionControl: false
+
+        var map = L.map('map').setView([51.505, -0.09], 13);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+        L.marker([51.5, -0.09]).addTo(map)
+            .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+            .openPopup();
+    }
+  }
+}
+</script>
+
+<style>
+.row{
+  margin: 0px !important;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #ffffff;
+}
+#map.col-md-12.map.leaflet-container.leaflet-touch.leaflet-fade-anim.leaflet-grab.leaflet-touch-drag.leaflet-touch-zoom {
+    margin: 0 !important;
+    width: 100% !important;
+    height: 100% !important;
+    min-height: 900px !important;
+}
+
+@media (max-width: 620px){
+    
+}
+
+</style>
---
+++ src/components/navbar/main.vue
@@ -0,0 +1,47 @@
+<template>
+        <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
+          <div class="container">
+            <img style="width: 180px;" src="../../img/logo.jpg" alt="logo da fire">
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+              <ul class="navbar-nav ml-auto my-2 my-lg-0">
+                <li class="nav-item">
+                  <a class="nav-link js-scroll-trigger" href="#about">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link js-scroll-trigger" href="#services">Reportar</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link js-scroll-trigger" href="#portfolio">Linha do tempo</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link js-scroll-trigger" href="#contact">Relatóros</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+</template>
+<script>
+export default {
+    data(){
+        return {
+
+        }
+    },
+    methods:{
+
+    }
+}
+</script>
+<style scoped>
+.navbar{
+  display: block;
+  background-color: rgb(255, 141, 2);
+}
+.nav-link{
+  color: azure !important;
+}
+</style>
